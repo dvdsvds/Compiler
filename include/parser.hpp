91468ff5f90e06f90e85d68d5c984492f102bc23
dvdsvds <kundr5@naver.com>
2025-12-30 22:42:08 +0900

    codes

--- include/parser.hpp
+++ include/parser.hpp
@@ -1,1 +1,20 @@
 #pragma once
+#include <vector>
+#include "token.hpp"
+#include "ast.hpp"
+
+class Parser {
+    private:
+        std::vector<TokenData> tokens;
+        size_t curr_pos;
+
+        Token peek();
+        Token advance();
+        bool check(Token type);
+        bool match(Token type);
+        bool isAtEnd();
+        void expect(Token type);
+    public:
+        Parser(const std::vector<TokenData>& tokens);
+        Program* parse();
+};
